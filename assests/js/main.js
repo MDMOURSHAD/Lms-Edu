f2f7dd20f6567516a77eeafa3b71fe3677e5018b
MDMOURSHAD <77190784+MDMOURSHAD@users.noreply.github.com>
2021-09-02 22:37:59 +0600

    Add files via upload

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,24 +1,64 @@
-// Preloader 
-function load(){
 
-	var loading = document.getElementsByClassName('preloader');
 
-	loading[0].style.display = "none";
-}
+jQuery(document).ready(function($) {
 
-$(document).ready(function(){
-	
-	// Scroll to Top
-	$(window).scroll(function(){
+        
+        
 
-		if($(this).scrollTop() > 100){
-			$('.scroll-top').fadeIn();
-		}
+$('.video').magnificPopup({
+            type: 'iframe',
+            // other options
+            iframe: {
+                markup: '<div class="mfp-iframe-scaler">' +
+                    '<div class="mfp-close"></div>' +
+                    '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                    '</div>',
 
-		else{
-			$('.scroll-top').fadeOut();
-		}
+                patterns: {
+                    youtube: {
+                        index: 'youtube.com/',
 
-	});
+                        id: 'v=',
 
+                        src: 'https://www.youtube.com/embed/%id%'
+                    },
+                },
+
+             srcAction: 'iframe_src', 
+            }
+        });
+
+        /*-- Mobile Menu Show & Hide --*/
+
+        $(".menu-toggle-btn").click(function(){
+            $(".main-menu").toggleClass("open-nav");
+        });
+
+        /*-- Mobile Menu Show & Hide --*/
+
+        $(".open-search-box").click(function(){
+            $(".search-box").toggleClass("search-box-show");
+        });
+
+        /*-- Menu Sticky --*/
+
+        var windows = $(window);
+        var sticky = $('.header-sticky')
+        windows.on('scroll', function() {
+            var scroll = windows.scrollTop();
+            if (scroll < 250) {
+                sticky.removeClass('stick');
+            } else {
+                sticky.addClass('stick');
+            }
+        });
+
+        /*-- Counter Up --*/
+
+        $('.counter').counterUp({
+            delay: 10,
+            time: 2000
+        });
+
+            
 });
