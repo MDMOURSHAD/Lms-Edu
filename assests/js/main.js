9b5e2a932714bae68e3d6f66dba1d1e8746917af
MDMOURSHAD <77190784+MDMOURSHAD@users.noreply.github.com>
2021-09-06 23:16:13 +0600

    Add files via upload

--- assests/js/main.js
+++ assests/js/main.js
@@ -33,4 +33,30 @@ $(document).ready(function(){
 		}
 
 	});
+
+	// Magnific Popup Video
+	$('.free_video_popup').magnificPopup({
+    type: 'iframe',
+    // other options
+    iframe: {
+        markup: '<div class="mfp-iframe-scaler">'+
+                  '<div class="mfp-close"></div>'+
+                  '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+                '</div>', 
+
+        patterns: {
+          youtube: {
+            index: 'youtube.com/', 
+
+            id: 'v=',
+
+            src: 'https://www.youtube.com/embed/%id%' 
+          },
+
+
+        },
+
+        srcAction: 'iframe_src', 
+      }
+  });
 });
