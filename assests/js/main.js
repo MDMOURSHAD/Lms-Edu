61060de96d0abebcca403a3eca8cf762ab89a163
MDMOURSHAD <77190784+MDMOURSHAD@users.noreply.github.com>
2021-09-11 22:58:01 +0600

    Add files via upload

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,69 +1,50 @@
-// Preloader 
-function load(){
 
-	var loading = document.getElementsByClassName('preloader');
+			// Preloader 
+			function load() {
+				var loading = document.getElementsByClassName('preloader');
+				loading[0].style.display = "none";
+			}
 
-	loading[0].style.display = "none";
-}
+			$(document).ready(function() {
 
-$(document).ready(function(){
-	
-	// TOGGLE SEARCH BAR
-    	$('.search-main').click(function(){
+				// TOGGLE SEARCH BAR
+				$('.search-main').click(function() {
+					$('.search-form-main').toggleClass('active-class');
+					$('.search-field').focus();
+				});
 
-    		$('.search-form-main').toggleClass('active-class');
-    		$('.search-field').focus();
-    	});
-    	
-	// Sticky Menu
-	$(window).scroll(function(){
+				// Sticky Menu
+				$(window).scroll(function() {
+					if($(this).scrollTop() > 100) {
+						$('.nav').addClass('sticky');
+					} else {
+						$('.nav').removeClass('sticky');
+					}
+				});
 
-		if($(this).scrollTop() > 100){
-			$('.nav').addClass('sticky');
-		}
-
-		else{
-			$('.nav').removeClass('sticky');
-		}
-
-	});
-	
-	// Scroll to Top
-	$(window).scroll(function(){
-
-		if($(this).scrollTop() > 100){
-			$('.scroll-top').fadeIn();
-		}
-
-		else{
-			$('.scroll-top').fadeOut();
-		}
-
-	});
-
-	// Magnific Popup Video
-	$('.free_video_popup').magnificPopup({
-    type: 'iframe',
-    // other options
-    iframe: {
-        markup: '<div class="mfp-iframe-scaler">'+
-                  '<div class="mfp-close"></div>'+
-                  '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-                '</div>', 
-
-        patterns: {
-          youtube: {
-            index: 'youtube.com/', 
-
-            id: 'v=',
-
-            src: 'https://www.youtube.com/embed/%id%' 
-          },
-
-
-        },
-
-        srcAction: 'iframe_src', 
-      }
-  });
-});
+				// Scroll to Top
+				$(window).scroll(function() {
+					if($(this).scrollTop() > 100) {
+						$('.scroll-top').fadeIn();
+					} else {
+						$('.scroll-top').fadeOut();
+					}
+				});
+				
+				// Magnific Popup Video
+				$('.free_video_popup').magnificPopup({
+					type: 'iframe',
+					// other options
+					iframe: {
+						markup: '<div class="mfp-iframe-scaler">' + '<div class="mfp-close"></div>' + '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' + '</div>',
+						patterns: {
+							youtube: {
+								index: 'youtube.com/',
+								id: 'v=',
+								src: 'https://www.youtube.com/embed/%id%'
+							},
+						},
+						srcAction: 'iframe_src',
+					}
+				});
+			});
